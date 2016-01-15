f5d5ede20f1711a60b8e777ab3fe263ccd46ae00
DadiHall <dadih32@gmail.com>
2016-01-15 01:37:50 +0000

    Finished building CV, added some css for the CV, some minor modifications are still left to do

---
+++ app/controllers/cv_controller.rb
@@ -0,0 +1,18 @@
+class CvController < ApplicationController
+
+	def education
+	end
+
+	def exhibition
+	end
+
+	def activities
+	end
+
+	def membership
+	end
+
+	def grants
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
 
+
+
   root 'welcome#index'
 
   get 'welcome/about' => 'welcome#about'
 
   get 'welcome/news' => 'welcome#news'
-  
-end
+
+	get 'cv/education' => 'cv#education'	
+
+	get 'cv/exhibition' => 'cv#exhibition'  
+
+	get 'cv/activities' => 'cv#activities'
+
+	get 'cv/membership' => 'cv#membership'
+
+	get 'cv/grants'		=> 'cv/grants'
+ end
